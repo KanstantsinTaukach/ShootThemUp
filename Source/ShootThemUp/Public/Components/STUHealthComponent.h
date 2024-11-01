4ae47eb7217ed4a8661f2b5159278e667fcf044c
Kanstantsin Taukach <tovkachkot@gmail.com>
2024-11-01 17:18:27 +0300

    added WBP_PlayerHUD (BlueprintWidget)

--- Source/ShootThemUp/Public/Components/STUHealthComponent.h
+++ Source/ShootThemUp/Public/Components/STUHealthComponent.h
@@ -18,11 +18,17 @@ class SHOOTTHEMUP_API USTUHealthComponent : public UActorComponent
     FOnDeathSignature OnDeath;
     FOnHealthChangedSignature OnHealthChanged;
 
-    UFUNCTION(BlueprintCallable)
+    UFUNCTION(BlueprintCallable, Category = "Health")
     bool IsDead() const
     {
         return FMath::IsNearlyZero(Health);
     };
+
+    UFUNCTION(BlueprintCallable, Category = "Health")
+    float GetHealthPercent() const
+    {
+        return Health / MaxHealth;
+    }
 
     float GetHealth() const
     {
