a411fc2e7f68f2dab7735d334f067b707130e9c7
Kanstantsin Taukach <tovkachkot@gmail.com>
2025-03-06 16:44:54 +0300

    added STUAIPerceptionComponent to the STUAIController

--- Source/ShootThemUp/Public/AI/STUAIController.h
+++ Source/ShootThemUp/Public/AI/STUAIController.h
@@ -6,11 +6,21 @@
 #include "CoreMinimal.h"
 #include "STUAIController.generated.h"
 
+class USTUAIPerceptionComponent;
+
 UCLASS()
 class SHOOTTHEMUP_API ASTUAIController : public AAIController
 {
     GENERATED_BODY()
 
+  public:
+    ASTUAIController();
+
   protected:
+    UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Components")
+    USTUAIPerceptionComponent *STUAIPerceptionComponent;
+
+    virtual void Tick(float DeltaTime) override;
+
     virtual void OnPossess(APawn *InPawn) override;
 };
